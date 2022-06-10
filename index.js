d2e93160a9c41c79d4fd5555d09db5e96e01edfa
Alison Blomenberg <sheesania@users.noreply.github.com>
2022-06-10 12:41:30 -0600

    Basic functionality working and tested

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
-module.exports = () => {
-  console.log("nina mawmaw");
+const fs = require("fs");
+const path = require("path");
+const Freemarker = require("freemarker");
+
+module.exports = (configuration, namespace, done) => {
+  const fragmentConfig = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "fragment.json"))
+  );
+  const { cssPath, htmlPath, jsPath } = fragmentConfig;
+
+  const css = fs.readFileSync(path.join(__dirname, cssPath));
+  const style = document.createElement("style");
+  style.innerHTML = css;
+  document.head.appendChild(style);
+
+  const html = fs.readFileSync(path.join(__dirname, htmlPath));
+  const freemarker = new Freemarker({ tagSyntax: "squareBracket" });
+  freemarker.render(
+    html,
+    {
+      fragmentEntryLinkNamespace: namespace,
+      configuration: configuration,
+    },
+    (err, freemarkerResult) => {
+      if (err) {
+        throw new Error(err);
+      }
+
+      const div = document.createElement("div");
+      div.innerHTML = freemarkerResult;
+      document.body.appendChild(div);
+
+      window.fragmentElement = div;
+      window.fragmentNamespace = namespace;
+      window.configuration = configuration;
+      require("./" + jsPath);
+      done();
+    }
+  );
 }
---
+++ index.test.js
@@ -0,0 +1,32 @@
+const { screen } = require("@testing-library/dom");
+const setupLiferayFragmentTest = require("./index.js");
+
+const testConfiguration = { numberOfTabs: 4 };
+
+describe("setup-liferay-fragment-test", () => {
+  beforeAll((done) => {
+    setupLiferayFragmentTest(testConfiguration, "mock-namespace", done);
+  });
+
+  it("loads CSS", () => {
+    const firstTab = screen.getAllByText("Hello I am a tab")[0];
+    expect(getComputedStyle(firstTab).color).toBe("blue");
+  });
+
+  it("loads HTML, including Freemarker directives and variable substitutions", () => {
+    const tabs = screen.getAllByRole("tabpanel");
+    expect(tabs.length).toBe(4);
+    expect(tabs[0].getAttribute("data-fragment-namespace")).toBe("mock-namespace");
+    expect(tabs[0].getAttribute("id")).toBe("tabPanel1");
+  });
+
+  it("passes appropriate globals to JS", () => {
+    expect(fragmentElement).toBeDefined();
+    expect(fragmentNamespace).toBe("mock-namespace");
+    expect(configuration).toBe(testConfiguration);
+  });
+
+  it("loads JS", () => {
+    expect(configuration.testMainWasLoaded).toBe(true);
+  });
+})
